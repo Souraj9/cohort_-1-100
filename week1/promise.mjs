ed588ead6644366bb5d5e361185edec3aeb3e180
Souraj <121894123+Souraj9@users.noreply.github.com>
2024-07-20 23:59:54 +0530

    async await

--- week1/promise.mjs
+++ week1/promise.mjs
@@ -4,14 +4,28 @@ import fs from "fs";
 const readFile =()=>{
     return new Promise(resolve => {
        fs.readFile("text.txt", "utf8", (err, data)=>{
-        setTimeout(resolve(data),1000);
+        setTimeout(()=>resolve(data),1000);
         });
     })
 }
 
-const afterFileRead =(data)=>{
-    console.log(data);
+
+// promise :
+// const afterFileRead =(data)=>{
+//     console.log(data);
+// }
+
+// readFile().then(afterFileRead);
+
+
+// async await :
+
+const afterFileRead = async() => {
+    let newData = await readFile();
+    console.log(newData);
 }
 
+afterFileRead();
 
-readFile().then(afterFileRead);
+
+
